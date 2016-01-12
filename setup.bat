c33bac4bcbd791c917c079e91daabee44f1cffc2
Peter Halliday <pghalliday@gmail.com>
2016-01-13 00:49:34 +0100

    add python

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install -y conemu
+choco install -y python2
 choco install -y vim
 choco install -y git
 choco install -y unxutils
