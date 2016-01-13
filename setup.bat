ab18566b59025cc3a269f3ca6f3fcf32d33f6f64
Peter Halliday <pghalliday@gmail.com>
2016-01-13 10:31:14 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,3 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install -y conemu
 choco install -y python2
 choco install -y vim
@@ -8,4 +7,4 @@ choco install -y poshgit
 choco install -y clink
 choco install -y tortoisesvn
 choco install -y tortoisegit
-choco install -y subversion
+choco install -y svn
